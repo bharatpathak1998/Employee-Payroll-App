e236f81908b5f827ecbc4f409fa7fc68d8741a50
bharatpathak1998 <bharatpathak099@gmail.com>
2022-10-18 20:16:03 +0530

    Added : Functionality To Set Event Listener On Salary Range To Display

---
+++ Js/EmployeePayroll.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const salary = document.querySelector("#salary");
+    const output = document.querySelector('.salary-output');
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
+})
